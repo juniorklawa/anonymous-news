dc54af1349641dfc53b46b347f8a6a1aa9c8a1d7
ejwindows <juniorklawa10@gmail.com>
2020-02-02 16:19:04 -0300

    finished React project

---
+++ build/precache-manifest.9e32fe91f9f20acd255875b6ce9e6dab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "86f9a01e6eb2c03a45f6",
+    "url": "/static/js/main.cdb0d68e.chunk.js"
+  },
+  {
+    "revision": "4c5c445e52eae7d6dd82",
+    "url": "/static/js/2.3378b96e.chunk.js"
+  },
+  {
+    "revision": "86f9a01e6eb2c03a45f6",
+    "url": "/static/css/main.a8963974.chunk.css"
+  },
+  {
+    "revision": "f5fc35e692634f4fd67162941b24a5ae",
+    "url": "/index.html"
+  }
+];
--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -1,23 +1,26 @@
 import React from "react";
 import useFormValidation from "../Auth/useFormValidation";
 import validateCreateLink from "../Auth/validateCreateLink";
-import { FirebaseContext } from "../../firebase";
+import FirebaseContext from "../../firebase/context";
 
 const INITIAL_STATE = {
   description: "",
   url: ""
-}
-
+};
 
 function CreateLink(props) {
-  const { firebase, user } = React.useContext(FirebaseContext)
-  const { handleSubmit, handleChange, values, errors } = useFormValidation(INITIAL_STATE, validateCreateLink, handleCreateLink)
+  const { firebase, user } = React.useContext(FirebaseContext);
+  const { handleSubmit, handleChange, values, errors } = useFormValidation(
+    INITIAL_STATE,
+    validateCreateLink,
+    handleCreateLink
+  );
 
   function handleCreateLink() {
     if (!user) {
-      props.history.push('/login')
+      props.history.push("/login");
     } else {
-      const { url, description } = values
+      const { url, description } = values;
       const newLink = {
         url,
         description,
@@ -25,12 +28,13 @@ function CreateLink(props) {
           id: user.uid,
           name: user.displayName
         },
+        voteCount: 0,
         votes: [],
         comments: [],
         created: Date.now()
-      }
-      firebase.db.collection('links').add(newLink)
-      props.history.push('/');
+      };
+      firebase.db.collection("links").add(newLink);
+      props.history.push("/");
     }
   }
 
@@ -43,26 +47,22 @@ function CreateLink(props) {
         placeholder="A description for your link"
         autoComplete="off"
         type="text"
-        className={errors.description && 'error-input'}
+        className={errors.description && "error-input"}
       />
       {errors.description && <p className="error-text">{errors.description}</p>}
       <input
-        name="url"
         onChange={handleChange}
         value={values.url}
+        name="url"
         placeholder="The URL for the link"
         autoComplete="off"
-        type="text"
-        className={errors.url && 'error-input'}
+        type="url"
+        className={errors.url && "error-input"}
       />
       {errors.url && <p className="error-text">{errors.url}</p>}
-      <button
-        className="button"
-        type="submit"
-      >
+      <button className="button" type="submit">
         Submit
       </button>
-
     </form>
   );
 }
